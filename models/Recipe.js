643f58aa2d4774bcaaef439863fcdfcd3669b2a5
Michael Ball <47047399+mball028@users.noreply.github.com>
2020-05-11 23:01:26 -0600

    Recipe model

---
+++ models/Recipe.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  directions: {
+    type: [String],
+    required: true,
+  },
+});
+
+const Recipe = mongoose.model("recipe", recipeSchema);
+module.exports = Recipe;
