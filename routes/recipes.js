3ef8eddfbc1f24474bef1f995e15d273da2d23c9
Michael Ball <47047399+mball028@users.noreply.github.com>
2020-05-12 11:55:42 -0600

    added edit and delete routes on recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -7,7 +7,13 @@ router.get("/", (req, res) => {
   let recipe_query = Recipe.find({});
   let recipe_promise = recipe_query.exec();
 
-  recipe_promise.then((recipes) => res.json(recipes)).catch((err) => res.send(err));
+  recipe_promise
+    .then((recipes) => {
+      recipes.length < 1
+        ? res.send("No recipes in the database.")
+        : res.send(recipes);
+    })
+    .catch((err) => res.send(err));
 });
 
 router.post("/add", (req, res) => {
@@ -20,6 +26,31 @@ router.post("/add", (req, res) => {
       res.json(recipe);
     })
     .catch((err) => res.send(err));
+});
+
+router.put("/editrecipe/:id", (req, res) => {
+  Recipe.findById(req.params.id)
+    .then((recipe) => {
+      recipe.title = req.body.title;
+      recipe.description = req.body.description;
+      recipe.directions = req.body.directions;
+
+      recipe
+        .save()
+        .then(() =>
+          res.json(`Recipe for ${recipe.title} updated successfully.`)
+        )
+        .catch((err) => res.json(`Error: ${err}`));
+    })
+    .catch((err) => res.json(`Error: ${err}`));
+});
+
+router.delete("/delete/:id", (req, res) => {
+  Recipe.findByIdAndDelete(req.params.id)
+    .then((recipe) => {
+      res.json(`${recipe.title} has been delted successfully.`);
+    })
+    .catch((err) => `Error: ${err}`);
 });
 
 module.exports = router;
